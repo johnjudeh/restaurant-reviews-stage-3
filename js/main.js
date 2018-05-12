e6e76442e4b6bc087360a3f872de2a6e3a3dbf40
John Judeh <johnjudeh@hotmail.com>
2018-05-12 13:53:36 +0100

    feat: Redesign home page of app for smallest screen width ~300px
     - Updated html documents with google maps API key
     - Updated html documents with classes according to BEM methodology
     - Updated main.js to add new classes to generated elements
     - Updated CSS for home page to target new classes rather than ids
     - Removed unnecessary CSS styles and grouped styles into shorthand properties
     - Recreated page design for better look on small device screens (iPhone 5)

--- js/main.js
+++ js/main.js
@@ -137,25 +137,30 @@ fillRestaurantsHTML = (restaurants = self.restaurants) => {
  */
 createRestaurantHTML = (restaurant) => {
   const li = document.createElement('li');
+  li.className = 'restaurants-list__card';
 
   const image = document.createElement('img');
-  image.className = 'restaurant-img';
+  image.className = 'restaurants-list__card__img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
   const name = document.createElement('h1');
+  name.className = 'restaurants-list__card__title';
   name.innerHTML = restaurant.name;
   li.append(name);
 
   const neighborhood = document.createElement('p');
+  neighborhood.className = 'restaurants-list__card__address';
   neighborhood.innerHTML = restaurant.neighborhood;
   li.append(neighborhood);
 
   const address = document.createElement('p');
+  address.className = 'restaurants-list__card__address';
   address.innerHTML = restaurant.address;
   li.append(address);
 
   const more = document.createElement('a');
+  more.className = 'restaurants-list__card__link';
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
   li.append(more)
