4c2e714af35601db806d2708bc084819f3f66c17
John Judeh <johnjudeh@hotmail.com>
2018-05-13 17:41:25 +0100

    feat: Add appropriate page headings for easy accessible navigation
     - Create only one h1 on each page (the website title)
     - Create hierachy in all other headings
     - Ensure each section has a descriptive title

--- js/main.js
+++ js/main.js
@@ -181,7 +181,7 @@ createRestaurantHTML = (restaurant) => {
   image.alt = '';
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h3');
   name.className = 'restaurants-list__card__title';
   name.innerHTML = restaurant.name;
   li.append(name);
